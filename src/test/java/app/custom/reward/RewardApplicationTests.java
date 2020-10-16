e6527a20802d902f74fbe6ea22c473a49c75ccb1
Eugene Bobalo <ebobalo@ArgonInd.biz>
2020-10-16 14:34:27 -0500

    started Reward Application with apache derby

---
+++ src/test/java/app/custom/reward/RewardApplicationTests.java
@@ -0,0 +1,13 @@
+package app.custom.reward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
