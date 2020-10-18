b555f3437fe921033e81702c8c267c6105b3def4
Eugene <eugenebobalo@gmail.com>
2020-10-17 19:44:41 -0500

    move RestController out

--- src/main/java/app/custom/reward/controller/HomeController.java
+++ src/main/java/app/custom/reward/controller/HomeController.java
@@ -1,40 +1,17 @@
 package app.custom.reward.controller;
 
-import app.custom.reward.dto.UserRepository;
-import app.custom.reward.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 
-@RestController
+@Controller
 public class HomeController {
 
-    @Autowired
-    private UserRepository repository;
-
-    @GetMapping("/")
+    @RequestMapping("/")
     public String homePage(Model model) {
         model.addAttribute("pageTitle", "Reward Application");
         return "index";
-    }
-
-    @PostMapping("/saveUsers")
-    public String saveUser(List<User> users) {
-        repository.saveAll(users);
-        return users.size() + "record saved..";
-    }
-
-    @GetMapping("/getAll")
-    public List<User> getAllUsers() {
-        return (List<User>) repository.findAll();
-    }
-
-    @GetMapping("/getUserByName/{name}")
-    public User findUserByName(@PathVariable String name) {
-        return repository.findByName(name);
     }
 
 }
---
+++ src/main/java/app/custom/reward/controller/RestHomeController.java
@@ -0,0 +1,34 @@
+package app.custom.reward.controller;
+
+import app.custom.reward.dto.UserRepository;
+import app.custom.reward.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RestHomeController {
+
+    @Autowired
+    private UserRepository repository;
+
+    @PostMapping("/saveUsers")
+    public String saveUser(List<User> users) {
+        repository.saveAll(users);
+        return users.size() + "record saved..";
+    }
+
+    @GetMapping("/getAll")
+    public List<User> getAllUsers() {
+        return (List<User>) repository.findAll();
+    }
+
+    @GetMapping("/getUserByName/{name}")
+    public User findUserByName(@PathVariable String name) {
+        return repository.findByName(name);
+    }
+}
