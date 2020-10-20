e1d3af3aa2c8d984abc1951de23d06e60b48ba1a
Eugene Bobalo <ebobalo@ArgonInd.biz>
2020-10-20 15:22:16 -0500

    500 error whene posting to output.html page

--- src/main/java/app/custom/reward/controller/HomeController.java
+++ src/main/java/app/custom/reward/controller/HomeController.java
@@ -1,17 +1,42 @@
 package app.custom.reward.controller;
 
+import app.custom.reward.entity.TestEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 @Controller
 public class HomeController {
 
+    LocalDateTime localDt = LocalDateTime.now();
+    DateTimeFormatter format = DateTimeFormatter.ofPattern("MM-dd-yyyy"); // dd-MM-yyyy HH:mm
+    String formatDateTime = localDt.format(format);
+
     @RequestMapping("/")
     public String homePage(Model model) {
         model.addAttribute("pageTitle", "Reward Application");
+        model.addAttribute("timeNow", formatDateTime);
         return "index";
+    }
+
+    @GetMapping("/testinput")
+    public String sendForm(Model model) {
+        model.addAttribute("user", new TestEntity());
+        return "testinput";
+    }
+
+    @PostMapping("/testinput")
+    public String processForm(@ModelAttribute TestEntity user, Model model) {
+        System.out.println("Inside of processForm method in controller");
+        return "output";
     }
 
 }
---
+++ src/main/java/app/custom/reward/entity/TestEntity.java
@@ -0,0 +1,13 @@
+package app.custom.reward.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class TestEntity {
+
+    private String name;
+    private String occupation;
+
+}
